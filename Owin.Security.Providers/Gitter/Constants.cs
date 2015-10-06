586149473f954368359f54bad732232836f7f35b
Jesse Hallam <jesse.colin.hallam@gmail.com>
2015-10-06 08:10:31 -0400

    Pruned unnecessary providers.

--- Owin.Security.Providers/Gitter/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Owin.Security.Providers.Gitter
-{
-    internal static class Constants
-    {
-        public const string DefaultAuthenticationType = "Gitter";
-    }
-}
--- Owin.Security.Providers/Onshape/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Owin.Security.Providers.Onshape
-{
-    internal static class Constants
-    {
-        public const string DefaultAuthenticationType = "Onshape";
-    }
-}
